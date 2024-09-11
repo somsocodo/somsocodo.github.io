053ce296da7287c6be1e943fb3a134048e27c615
Cosmos Bar-Even <odosomsoc@gmail.com>
2024-09-11 17:00:08 +1200

    loading

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,4 +1,11 @@
-import { Button, Card, Grid2 as Grid, CardMedia, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  Grid2 as Grid,
+  CardMedia,
+  Typography,
+  CircularProgress
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 
 interface Sound {
@@ -51,33 +58,25 @@ export function Track({ trackid, play, style }: Props) {
             {sound?.artwork_url ? (
               <CardMedia component="img" sx={{ width: '14vh' }} image={sound.artwork_url} />
             ) : (
-              <CardMedia
-                component="img"
-                sx={{ width: '14vh' }}
-                image={
-                  'https://developers.soundcloud.com/assets/powered_by_white-371bd6967352fcc89673d4c81f7e5661.png'
-                }
-              />
+              <CircularProgress size={'14vh'} />
             )}
           </Grid>
-          <Grid size="grow">
-            <div style={{ paddingLeft: '10' }}>
-              <Typography component="div" fontSize={'3vh'} sx={{ color: 'whitesmoke' }} noWrap>
-                {sound?.title}
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                component="div"
-                fontSize={'2vh'}
-                sx={{ color: 'whitesmoke' }}
-                noWrap
-              >
-                {sound?.user.username}
-              </Typography>
-              <Button onClick={() => play(trackid)} style={{ width: '2vh' }}>
-                Play
-              </Button>
-            </div>
+          <Grid size="grow" paddingLeft={'10px'} paddingTop={'0.5vh'}>
+            <Typography component="div" fontSize={'3vh'} sx={{ color: 'whitesmoke' }} noWrap>
+              {sound ? sound?.title : '...'}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              component="div"
+              fontSize={'2vh'}
+              sx={{ color: 'whitesmoke' }}
+              noWrap
+            >
+              {sound ? sound?.user.username : '...'}
+            </Typography>
+            <Button onClick={() => play(trackid)} style={{ width: '2vh' }} disabled={!sound}>
+              Play
+            </Button>
           </Grid>
           <Grid size="grow" maxWidth={'15vh'}>
             <a href={sound?.permalink_url ? sound?.permalink_url : 'https://soundcloud.com/'}>
