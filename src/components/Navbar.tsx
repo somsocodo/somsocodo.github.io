0d3f495949bead47ff5f855670d88259042c91cb
Cosmos Bar-Even <odosomsoc@gmail.com>
2024-09-16 09:59:15 +1200

    mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,97 +1,110 @@
-import { HistoryEduRounded, MusicNoteRounded } from '@mui/icons-material';
-import { AppBar, Button, Grid2 as Grid, ListItemIcon, Menu, MenuItem, SvgIconTypeMap, Typography } from '@mui/material';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { HistoryEduRounded, MenuRounded, MusicNoteRounded } from '@mui/icons-material';
+import {
+  AppBar,
+  Drawer,
+  Grid2 as Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography
+} from '@mui/material';
 import { ReactElement, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useWindowDimensions from '../helpers/windowDimensions';
 
 interface Route {
-  label: string
-  route: string
-  icon: ReactElement<any, any>
+  label: string;
+  route: string;
+  icon: ReactElement;
 }
-
 
 const routes: Route[] = [
   {
     label: 'music',
     route: '/music',
-    icon: <MusicNoteRounded/>
+    icon: <MusicNoteRounded />
   },
   {
     label: 'projects',
     route: '/projects',
-    icon: <HistoryEduRounded/>
+    icon: <HistoryEduRounded />
   }
-]
+];
 
 export function NavBar() {
-  const { height, width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  
+
   return (
-    <AppBar position="sticky" sx={{ backgroundColor: '#17191a', height: '10vh', width: '100%' }}>
+    <AppBar
+      position="sticky"
+      sx={{ backgroundColor: '#17191a', height: '30', width: '100%', overflow: 'visible' }}
+    >
       <Grid container spacing={'1vw'}>
         <Grid padding={'2vh'} size="auto" alignContent={'center'}>
-          <Typography component="div" fontSize={'3vh'} sx={{ color: 'whitesmoke' }} noWrap>
+          <Typography component="div" sx={{ color: 'whitesmoke' }} noWrap>
             somsocodo.github.io
           </Typography>
         </Grid>
-        { width > 600 ? (routes.map((route)=>{
-          return (
-            <Grid padding={'2vh'} size="auto" alignContent={'center'}>
-            <Button href={route.route} onClick={() => navigate('/music')} style={{ width: '2vh' }} endIcon={route.icon}>
-              {route.label}
-            </Button>
-          </Grid>
-          )
-        })) : (
-          <><Grid size={'grow'} />
-          <Grid padding={'2vh'} size="auto">
-              <div>
-                <Button
-                  id="basic-button"
-                  aria-controls={open ? 'basic-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? 'true' : undefined}
-                  onClick={handleClick}
-                  sx={{marginRight: 0}}
-                  
-                >
-                  Dashboard
-                </Button>
-                <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    'aria-labelledby': 'basic-button',
+        {width > 600 ? (
+          routes.map((route) => {
+            return (
+              <Grid key={route.label} padding={'2vh'} size="auto" alignContent={'center'}>
+                <ListItemButton
+                  href={route.route}
+                  onClick={() => {
+                    navigate(route.route);
                   }}
-                  sx={{marginRight: 40}}
                 >
-                 {routes.map((route)=>{
-                  return ( 
-                  <MenuItem onClick={handleClose}>
-                    Profile
-                    <ListItemIcon>{route.icon}</ListItemIcon>
+                  <ListItemIcon sx={{ color: 'whitesmoke', minWidth: 30 }}>
+                    {route.icon}
+                  </ListItemIcon>
+                  <ListItemText primary={route.label} />
+                </ListItemButton>
+              </Grid>
+            );
+          })
+        ) : (
+          <>
+            <Grid size={'grow'} />
 
-                  </MenuItem>)
-                 })}
-                </Menu>
-              </div>
-            </Grid></>
+            <div>
+              <IconButton sx={{ paddingLeft: 10 }} onClick={toggleDrawer(true)}>
+                <MenuRounded fontSize="large" htmlColor="white" />
+              </IconButton>
+              <Drawer anchor={'right'} open={open} onClose={toggleDrawer(false)}>
+                <List sx={{ backgroundColor: '#17191a', height: '100%' }}>
+                  {routes.map((route) => {
+                    return (
+                      <ListItem key={route.label} sx={{ color: 'whitesmoke' }}>
+                        <ListItemButton
+                          href={route.route}
+                          onClick={() => {
+                            navigate(route.route);
+                          }}
+                        >
+                          <ListItemIcon sx={{ color: 'whitesmoke', minWidth: 30 }}>
+                            {route.icon}
+                          </ListItemIcon>
+                          <ListItemText primary={route.label} />
+                        </ListItemButton>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </Drawer>
+            </div>
+          </>
         )}
-        </Grid>
+      </Grid>
     </AppBar>
   );
 }
