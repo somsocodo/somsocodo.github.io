6f832a63557b8d9378cd9a5c40f99c8b9e784086
Cosmos Bar-Even <odosomsoc@gmail.com>
2025-03-03 17:49:29 +1300

    next.js

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+	/* config options here */
+	output: 'export',
+	distDir: 'build',
+	reactStrictMode: true
+};
+
+export default nextConfig;
